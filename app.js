53fa4f96190dfc3cc1c0e8c9ae0750de3fbd61df
Frederik <Frederiksalling8@hotmail.com>
2020-09-25 10:38:29 +0200

    Added account reg

--- app.js
+++ app.js
@@ -2,14 +2,31 @@ const express = require('express');
 const path = require('path');
 const routes = require('./routes/index');
 const bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+mongoose.connect("mongodb://localhost:27017/Fitness");
+var db = mongoose.connection;
+// mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// use sessions for tracking logins
+app.use(session({
+    secret: 'very secret secret',
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: db
+    })
+  }));
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
-
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,105 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const auth = require('http-auth');
 const { check, validationResult } = require('express-validator');
-
+var mid = require('../middleware');
+var User = require('../models/user');
 const router = express.Router();
-const Registration = mongoose.model('Registration');
 
-router.get('/', (req, res) => {
-    res.render('form', { title: 'Registration form' });
-  });
 
-router.get('/registrations', (req, res) => {
-  Registration.find()
-    .then((registrations) => {
-      res.render('index', { title: 'Listing registrations', registrations });
-    })
-    .catch(() => { res.send('Sorry! Something went wrong.'); });
+// GET /workout
+router.get('/workout', mid.requiresLogin, function(req, res, next) {
+  User.findById(req.session.userId)
+      .exec(function (error) {
+        if (error) {
+          return next(error);
+        } else {
+          return res.render('workout');
+        }
+      });
 });
 
-router.post('/',
-[
-    check('name')
-    .isLength({ min: 1 })
-    .withMessage('Please enter a name'),
-  check('email')
-    .isLength({ min: 1 })
-    .withMessage('Please enter an email'),
-],
-(req, res) => {
-  console.log(req.body);
-    const errors = validationResult(req);
-    if (errors.isEmpty()) {
-      const registration = new Registration(req.body);
-      registration.save()
-        .then(() => { res.send('Thank you for your registration!'); })
-        .catch((err) => {
-          console.log(err);
-          res.send('Sorry! Something went wrong.');
-        });
-    } else {
-      res.render('form', {
-        title: 'Registration form',
-        errors: errors.array(),
-        data: req.body,
-      });
-    }
+
+// GET /logout
+router.get('/logout', function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) { 
+      if(err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
   }
-);
+});
+// GET /login
+router.get('/login', mid.loggedOut, function(req, res, next) {
+  return res.render('login', { title: 'Log In'});
+});
+
+// POST /login
+router.post('/login', function(req, res, next) {
+  if (req.body.email && req.body.password) {
+    User.authenticate(req.body.email, req.body.password, function (error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      }  else {
+        req.session.userId = user._id;
+        return res.redirect('/workout');
+      }
+    });
+  } else {
+    var err = new Error('Email and password are required.');
+    err.status = 401;
+    return next(err);
+  }
+});
+
+// GET /register
+router.get('/register', mid.loggedOut, function(req, res, next) {
+  return res.render('register', { title: 'Sign Up' });
+});
+
+// POST /register
+router.post('/register', function(req, res, next) {
+  if (req.body.email &&
+    req.body.password &&
+    req.body.confirmPassword) {
+
+      // confirm that user typed same password twice
+      if (req.body.password !== req.body.confirmPassword) {
+        var err = new Error('Passwords do not match.');
+        err.status = 400;
+        return next(err);
+      }
+
+      // create object with form input
+      var userData = {
+        email: req.body.email,
+        password: req.body.password
+      };
+
+      // use schema's `create` method to insert document into Mongo
+      User.create(userData, function (error, user) {
+        if (error) {
+          return next(error);
+        } else {
+          req.session.userId = user._id;
+          return res.redirect('/workout');
+        }
+      });
+
+    } else {
+      var err = new Error('All fields required.');
+      err.status = 400;
+      return next(err);
+    }
+})
+
+// GET /
+router.get('/', function(req, res, next) {
+  return res.render('index', { title: 'Home' });
+});
 
 module.exports = router;
