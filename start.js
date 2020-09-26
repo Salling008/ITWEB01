c5c88fb5effd06e8ad4e0ec53a43a11700960ca3
Rasmus Duedahl Vesterheim <201404212@post.au.dk>
2020-09-26 14:48:15 +0200

    ændret port og path til start

--- start.js
+++ start.js
@@ -1,4 +1,5 @@
 const app = require('./app');
-const server = app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
